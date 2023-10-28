dad6e337edef2d68e435ae8d1e56982b3bcc70c5
AdamZWinter <adam@bootstrapmusic.com>
2023-10-27 21:45:55 -0700

    Done.

--- src/main/resources/static/scripts/load_icons.js
+++ src/main/resources/static/scripts/load_icons.js
@@ -1,8 +1,8 @@
 //wait until the page loads!
 window.onload = async function() {
-    //await fetchIcons();
+    await fetchIcons();
 
-    addCards()
+    //addCards();
 
     //set an event handler for submitting a new joke
     // let addJokeButton = document.querySelector("button");
@@ -11,13 +11,35 @@ window.onload = async function() {
     // console.log("onload() ended");
 };
 
-function addCards()
+async function fetchIcons()
+{
+    let uri = "http://localhost:8080/api/v1/icons";
+    let config = {
+        method: "get"
+    };
+    let response = await fetch(uri, config);
+    let json = await response.json();
+    addCards(json);
+
+    /*    fetch(uri, config)
+            .then(function(response) {
+                console.log(response);
+                return response.json();
+            })
+            .then(function(json) {
+                console.log(json);
+            });*/
+}
+
+function addCards(json)
 {
 
     let section = document.querySelector("#icon-grid");
-    for (let i = 0; i < 25; i++)
+    for (let i = 0; i < json.length; i++)
     {
-        addIcon(section);
+        //let iconJson = JSON.parse(json[i]);
+        addIcon(section, json[i].imagePath);
+        //addIcon(section, iconJson.id);
         //let joke = jokesArray[i];
         //addSingleJoke(joke, section);
 
@@ -31,10 +53,11 @@ function addCards()
     }
 }
 
-function addIcon(section)
+function addIcon(section, id)
 {
     //create HTML elements
     let div = document.createElement("div");
+    let img = document.createElement("img");
     //let h1 = document.createElement("h1");
     //let p = document.createElement("p");
 
@@ -44,9 +67,13 @@ function addIcon(section)
 
     //add text or HTML attributes
     //h1.textContent = `Joke #${joke.id}`;
+    //h1.textContent = id;
     //p.textContent = joke.jokeText;
     div.className = "card";
+    img.src = id;
 
     //add the div to the section
+    //div.appendChild(h1);
+    div.appendChild(img);
     section.appendChild(div);
 }
